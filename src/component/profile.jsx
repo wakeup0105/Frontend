ca60e02edeec7f42d041ca58615d5e871456e7a1
jeongmin <mmaazz1000@naver.com>
2024-07-28 02:24:17 +0900

    메인페이지 작업

--- src/component/profile.jsx
+++ src/component/profile.jsx
@@ -1,50 +1,102 @@
 import React from 'react';
 import '../index.css';
-import { useLocation } from 'react-router-dom';
-import ilonaImage from '../image/ilona.png'; 
-import alone from '../image/alone.png';
-import enter from '../image/enter.png';
-import chat from '../image/chat.png';
-import shop from '../image/shop.png';
+import main from '../image/main.png';
+import health from '../image/health.png';
+import ring1 from '../image/ring1.png';
+import setting from '../image/setting.png';
 import jam from '../image/jam.png';
-import bag from '../image/bag 2.jpg';
-import ring from '../image/ring 2.jpg';
-import setting from '../image/setting.jpg';
-import levelbar from '../image/levelbar.PNG';
+import character1 from '../image/character1.png';
+import character2 from '../image/character2.png';
 
 export default function Profile() {
-    const location = useLocation();
-    const nickname = location.state?.nickname || '닉네임 없음';
+  const LeftPanel = () => {
     return (
-        <div className="profile-page">
-            <div className="top-section">
-                <div className="top-left">
-                    <img src={levelbar} alt="levelbar" />
-                    <div className="nickname-container">
-                        {nickname}
-                        <div className="nickname-tooltip">{nickname}</div> {/* 툴팁 추가 */}
-                    </div>
-                </div>
-                <div className="top-right">
-                    <div className="icons">
-                        <button className="icon-button jam-button"><img src={jam} alt="jam" /></button>
-                        <button className="icon-button"><img src={bag} alt="bag" /></button>
-                        <button className="icon-button"><img src={ring} alt="ring" /></button>
-                        <button className="icon-button"><img src={setting} alt="setting" /></button>
-                    </div>
-                </div>
+      <div className="left-panel">
+        <div className="character">
+          <img src="/path-to-your-image.png" alt="Character" />
+          <div className="character-info">
+            <span>이로나</span>
+            <div className="health-bar">
+              <div className="health" style={{ width: '75%' }}></div>
             </div>
-
-            <div className="center">
-                <img src={ilonaImage} alt="Ilona" className="center-image" />
-            </div>
-
-            <div className="bottom-buttons">
-                <button className="button"><img src={enter} alt="Enter" /></button>
-                <button className="button"><img src={alone} alt="Alone" /></button>
-                <button className="button small"><img src={chat} alt="Chat" /></button>
-                <button className="button small"><img src={shop} alt="Shop" /></button>
-            </div>
+          </div>
         </div>
+        <div className="buttons">
+          <button>목</button>
+          <button disabled>허리</button>
+        </div>
+      </div>
     );
+  };
+
+  const RightPanel = () => {
+    return (
+      <div className="right-panel">
+        <Timer />
+        <GoalProgress />
+      </div>
+    );
+  };
+
+  const Timer = () => {
+    return (
+      <div className="timer">
+        <div className="work-timer">
+          <h2>WORK</h2>
+          <div className="time">00:20</div>
+        </div>
+        <div className="audio-player">
+          <span>재활의학과 교수...</span>
+          <div className="controls">
+            <button>Play</button>
+            <button>Pause</button>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const GoalProgress = () => {
+    return (
+      <div className="goal-progress">
+        <div className="stress-level">어제 보다 5회 더 스트레칭했어요!</div>
+        <div className="progress-bar">
+          <div className="progress" style={{ width: '60%' }}></div>
+        </div>
+        <div className="goal">목표에 60% 달성했어요 (30/50회)</div>
+      </div>
+    );
+  };
+
+  return (
+    <div className="container">
+      <div className="top">
+        <span>일오나</span>
+        <a href="#">Log out</a>
+      </div>
+      <div className="second-top">
+        <div className="character">
+          <img src={character1} alt="character1" />
+          <img src={character2} alt="character2" />
+        </div>
+        <div className="icon">
+          <button className="icon-button">
+            <img src={jam} alt="jam" />
+          </button>
+          <button className="icon-button">
+            <img src={health} alt="health" />
+          </button>
+          <button className="icon-button">
+            <img src={ring1} alt="ring1" />
+          </button>
+          <button className="icon-button">
+            <img src={setting} alt="setting" />
+          </button>
+        </div>
+      </div>
+      <div className="Main-Layout">
+        <img src={main} alt="main" />
+      </div>
+    </div>
+  );
 }
